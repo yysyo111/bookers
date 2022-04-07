c2a96d7be2b56d1467af5097daef9855500c1553
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-07 00:22:19 +0000

    [Add]controller View追加　ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  resources :books
+  get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
