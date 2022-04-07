621db5ac50d30dc0b9e90f049ec170a0bcf4e592
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-07 02:54:25 +0000

    [Add]新規投稿機能と保存機能の途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,12 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
 
   def index
   end
@@ -10,5 +16,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
