5b94a46c147b0156c4a3b3a721f01aa3fe690241
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-07 09:01:03 +0000

    [Add]編集機能　投稿機能　一覧画面　詳細画面　削除機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,36 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show', as: 'book'  #URL /books/1に該当
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
   get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
