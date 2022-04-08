bd11014b8770ef30dc9ce248928b8aba549e8c24
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-08 04:32:00 +0000

    [Finish]RSpecのテスト終了

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # patch 'books/:id' => 'books#update', as: 'update_book'
   # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
-  get '/top' => "homes#top"
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
